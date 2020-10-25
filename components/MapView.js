a189216c19a0b09e5db24a058f62a6175bd37e2a
Rick Blalock <rblalock@gmail.com>
2020-10-24 20:01:42 -0400

    Finish stubbing out map ideas

--- components/MapView.js
+++ components/MapView.js
@@ -5,10 +5,33 @@ mapboxgl.accessToken = process.env.MAPS_KEY;
 const MapView = () => {
 	const mapContainer = useRef();
 
+	const getWaypoint = async (address) => {
+		// Probably should do this every time....lulz
+		const MapBoxClientLib = (await import('@mapbox/mapbox-sdk')).default;
+		const MapBoxGeocodeClientLib = (await import('@mapbox/mapbox-sdk/services/geocoding')).default;
+		const mapboxClient = MapBoxClientLib({ accessToken: process.env.MAPS_KEY });
+		const geocodeService = MapBoxGeocodeClientLib(mapboxClient);
+		const coordsRequest = await geocodeService.forwardGeocode({
+			query: address,
+			limit: 1
+		}).send();
+
+		if (coordsRequest.body && coordsRequest.body.features && coordsRequest.body.features.length > 0) {
+			return coordsRequest.body.features[0].center;
+		}
+
+		return undefined;
+	};
+
 	const loadMap = async () => {
 		const nav = new mapboxgl.NavigationControl();
 		// ref. https://github.com/mapbox/mapbox-gl-directions/blob/master/API.md
-		const MapboxDirections = (await import('@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions')).default
+		const MapboxDirections = (await import('@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions')).default;
+
+		// Example of getting a waypoint from address
+		const waypoint1 = await getWaypoint('1000 James L Turnage Blvd, West Palm Beach, FL 33415');
+		const waypoint2 = await getWaypoint('1 S County Rd, Palm Beach, FL 33480');
+
 		const directions = new MapboxDirections({
 			accessToken: process.env.MAPS_KEY,
 			unit: 'imperial',
@@ -17,19 +40,20 @@ const MapView = () => {
 		});
 		const map = new mapboxgl.Map({
 			container: mapContainer.current,
-			style: 'mapbox://styles/mapbox/dark-v10',
+			style: 'mapbox://styles/mapbox/light-v10',
 			antialias: true,
 			center: [-80.1, 26.7],
 			zoom: 13,
 			pitch: 45,
 		});
 		map.addControl(nav, 'top-left');
-		map.addControl(directions, 'top-right');
+		map.addControl(directions, 'bottom-right');
 
+		// Add the stops to the route
 		map.once('load', () => {
 			directions.setOrigin('500 N US-1, Tequesta, FL 33469');
-			directions.addWaypoint(0, [-80.1, 26.7]);
-			directions.addWaypoint(1, [-80.11, 26.71]);
+			directions.addWaypoint(0, waypoint1);
+			directions.addWaypoint(1, waypoint2);
 			directions.setDestination('500 N US-1, Tequesta, FL 33469');
 		});
 	};
